ca0841c8f9421842565cbc27180cbc1a9aaa1eae
Mohamed Amine Akacha <35059503+aminefreelancer@users.noreply.github.com>
2020-04-20 22:56:26 +0100

    Design Repository Component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Repo/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Repo from '@/components/Repo.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Repo
   }
 }
 </script>
 
 <style>
+html {
+  -webkit-text-size-adjust: 100%;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+body {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 16px;
+  line-height: 1.5;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50; 
+  padding: 20px;
+}
+.-shadow {
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.13);
 }
 </style>
---
+++ src/components/Repo.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="repo-item -shadow">
+		<div class="repo-img">
+			<img src="https://avatars1.githubusercontent.com/u/35059503?v=4">
+		</div>
+		<div class="repo-info">
+			<h2>Repo name</h2>
+			<p>Repo description Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure eius exercitationem aperiam. Ea possimus dolores assumenda harum odit. Rerum nesciunt culpa ut eveniet quos officiis laboriosam esse? Quidem, alias rerum!</p>
+			<hr>
+			<button type="button" class="btn btn-sm btn-primary">Stars: 118k</button>
+			<button type="button" class="btn btn-sm btn-danger">Issues: 1.6k</button>
+			<span>Submitted 30 days ago by aminefreelancer</span>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style scoped>
+.repo-item {
+	display: flex;
+	padding: 20px;
+	margin-bottom: 24px;
+	transition: all 0.2s linear;
+}
+.repo-item:hover {
+	transform: scale(1.01);
+	box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.repo-img {
+	flex-direction: column;
+}
+.repo-info {
+	flex-direction: column;
+	margin-left: 40px
+}
+.btn {
+	margin: 0px 5px;
+}
+</style>
